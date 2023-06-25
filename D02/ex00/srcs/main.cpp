730658fc4ebd91db1afc2d654e29e67cd5dce898
Tom GERNEZ <tgernez@student.42.fr>
2023-06-25 16:19:49 +0200

    added tests d02

--- D02/ex00/srcs/main.cpp
+++ D02/ex00/srcs/main.cpp
@@ -6,13 +6,20 @@
 /*   By: tgernez <tgernez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 17:47:41 by tgernez           #+#    #+#             */
-/*   Updated: 2023/06/23 11:56:54 by tgernez          ###   ########.fr       */
+/*   Updated: 2023/06/25 16:12:18 by tgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 #include <string>
+
+/*
+	On utilise des signed int 32 bits pour stocker les valeurs.
+	On donne 8 bits pour la partie decimale du nombre, la valeur maximale
+	pouvant don etre contenue est INT_32_SIGNED_MAX << 8 = 2147483647 << 8 = 8388607
+	Tout nombre superieur a 8388607 fera donc overflow la classe Fixed
+*/
 
 int main(void)
 {
--- D02/ex01/srcs/main.cpp
+++ D02/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tgernez <tgernez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 17:47:41 by tgernez           #+#    #+#             */
-/*   Updated: 2023/06/16 23:02:02 by tgernez          ###   ########.fr       */
+/*   Updated: 2023/06/25 16:17:22 by tgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ int main( void )
 {
 	Fixed a;
 	Fixed const b( 10 );
-	Fixed const c( 42.42f );
+	Fixed const c( -42.42f );
 	Fixed const d( b );
+	Fixed const e( 8388607 );
+	Fixed const f( 8388608 );
+	Fixed const g( -8388608 );
+	Fixed const h( -8388609 );
 	
 	a = Fixed( 1234.4321f );
 	
@@ -27,9 +31,27 @@ int main( void )
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+	std::cout << "e is " << e << " MAX" << std::endl;
+	std::cout << "f is " << f << " OVERFLOW MAX" << std::endl;
+	std::cout << "g is " << g << " MIN" << std::endl;
+	std::cout << "h is " << h << " OVERFLOW MIN" << std::endl;
+
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "e is " << e.toInt() << " as integer" << std::endl;
+	std::cout << "f is " << f.toInt() << " as integer" << std::endl;
+	std::cout << "g is " << g.toInt() << " as integer" << std::endl;
+	std::cout << "h is " << h.toInt() << " as integer" << std::endl;
+
+	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+	std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+	std::cout << "d is " << d.toFloat() << " as float" << std::endl;
+	std::cout << "e is " << e.toFloat() << " as float" << std::endl;
+	std::cout << "f is " << f.toFloat() << " as float" << std::endl;
+	std::cout << "g is " << g.toFloat() << " as float" << std::endl;
+	std::cout << "h is " << h.toFloat() << " as float" << std::endl;
 	return (0);
 }
