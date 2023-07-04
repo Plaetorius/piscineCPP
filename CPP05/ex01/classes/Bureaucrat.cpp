4b29e972d3d4d80794af7538342de303cc3488e1
Tag6362 <tag6362@gmail.com>
2023-07-04 17:05:15 +0200

    ex01

---
+++ CPP05/ex01/classes/Bureaucrat.cpp
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgernez <tgernez@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 18:07:36 by tgernez           #+#    #+#             */
+/*   Updated: 2023/07/04 15:55:18 by tgernez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+#include <iostream>
+
+Bureaucrat::Bureaucrat() : _name("Unknown"), _grade(150)
+{
+	std::cout << "Default Bureaucrat constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &obj): _name(obj._name), _grade(obj._grade)
+{
+	std::cout << "Copy Bureaucrat constructor called" << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat &rhs)
+{
+	std::cout << "Bureaucrat Assignment operator called" << std::endl;
+	if (this == &rhs)
+		return (*this);
+	// this->_name = rhs._name; Impossible because _name is const, only the grades are copied
+	this->_grade = rhs._grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat destructor called" << std::endl;
+}
+
+
+void	Bureaucrat::tryCatchGrade(int grade)
+{
+	if (grade < 1) 			throw (Bureaucrat::GradeTooHighException());
+	else if (grade > 150)	throw (Bureaucrat::GradeTooLowException());
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name)
+{
+	std::cout << "Bureaucrat constructor called for " << name
+				<< " with grade " << grade << std::endl;
+	tryCatchGrade(grade);
+	this->_grade = grade;
+}
+
+int	Bureaucrat::getGrade() const
+{
+	return (this->_grade);
+}
+
+const std::string Bureaucrat::getName() const
+{
+	return (this->_name);
+}
+
+void	Bureaucrat::promote()
+{
+	tryCatchGrade(this->_grade - 1);
+	this->_grade -= 1;
+	std::cout << this->_name << " has been promoted to grade " << this->_grade << "! Congrats!" << std::endl;
+}
+
+void	Bureaucrat::demote()
+{
+	tryCatchGrade(this->_grade + 1);
+	this->_grade += 1;
+	std::cout << this->_name << " has been demoted to grade " << this->_grade << "! Looser!" << std::endl;
+}
+
+void	Bureaucrat::signForm(Form &form)
+{
+	try
+	{
+		form.beSigned(*this);
+		std::cout << this->_name << " took his favorite pen and signed the form " << form.getName() << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << this->_name << " couldn't sign the form " << form.getName() << " because " << e.what() << std::endl;
+	}
+}
+
+std::ostream&	operator<<(std::ostream& os, const Bureaucrat &obj)
+{
+	os << obj.getName() << ", has grade " << obj.getGrade();
+	return (os);
+}
---
+++ CPP05/ex01/classes/Form.cpp
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgernez <tgernez@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/04 15:55:42 by tgernez           #+#    #+#             */
+/*   Updated: 2023/07/04 16:18:01 by tgernez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+Form::Form() : _name("Unknow Form"), _signed(false), _sign_grade(150), _exec_grade(150)
+{
+	std::cout << "Default Form constructor called" << std::endl;
+}
+
+Form::Form(const Form &obj) :  _name(obj._name), _signed(obj._signed), _sign_grade(obj._sign_grade), _exec_grade(obj._exec_grade)
+{
+	std::cout << "Copy Form constructor called" << std::endl;
+}
+
+Form& Form::operator=(const Form &rhs)
+{
+	std::cout << "Form Assignment operator called" << std::endl;
+	if (this == &rhs)
+		return (*this);
+	//All the other variables are consts
+	this->_signed = rhs._signed;
+	return (*this);
+}
+
+Form::~Form()
+{
+	std::cout << "Form destructor called" << std::endl;
+}
+
+Form::Form(const std::string& name, int sign_grade, int exec_grade) : _name(name), _signed(false), _sign_grade(sign_grade), _exec_grade(exec_grade)
+{
+	if (sign_grade > 150 || exec_grade > 150)
+		throw (Form::GradeTooLowException());
+	if (sign_grade < 1 || exec_grade < 1)
+		throw (Form::GradeTooHighException());
+}
+
+const std::string	Form::getName() const
+{
+	return (this->_name);
+}
+
+bool			Form::getSigned() const
+{
+	return (this->_signed);
+}
+int			Form::getSignGrade() const
+{
+	return (this->_sign_grade);
+}
+
+int			Form::getExecGrade() const
+{
+	return (this->_exec_grade);
+}
+
+void				Form::beSigned(const Bureaucrat& bureaucrat)
+{
+	const	int bureaucrat_grade = bureaucrat.getGrade();
+
+	std::cout << "beSigned called on form " << this->_name << std::endl;
+	if (this->_sign_grade < bureaucrat_grade)
+		throw Form::GradeTooLowException();
+	this->_signed = true;
+}
+
+std::ostream&	operator<<(std::ostream& os, const Form &obj)
+{
+	os << "Name: " << obj.getName() << std::endl
+		<< "Signed: " << obj.getSigned() << std::endl
+		<< "Sign Grade: " << obj.getSignGrade() << std::endl
+		<< "Exec Grade: " << obj.getExecGrade();
+	return (os);
+}
